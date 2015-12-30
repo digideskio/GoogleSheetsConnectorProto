d38832960f484722906bd93f76ae67762e95f2d7
lbrendanl <lee33@uw.edu>
2015-12-29 19:11:54 -0800

    finished v1 and 2 except thumbnail

--- sheetsV2.js
+++ sheetsV2.js
@@ -1,3 +1,5 @@
+var docIDs = [];
+
 $(document).ready(function() {
   showSpinner(true)
 
@@ -7,47 +9,12 @@ $(document).ready(function() {
   });
 
   $('#connectButton').prop('disabled', true);
-
-  $("#searchButton").click(function() { // This event fires when a button is clicked
-    searchSheets();
-  });
-
-  $('#searchBox').keypress(function (e) {
-    if (e.which == 13) {
-      searchSheets();
-      return false;
-    }
-  });
-
-  $("#titleHeader").click(function() { // This event fires when a button is clicked
-    fakeSortSetup();
-  });
-  $("#viewedHeader").click(function() { // This event fires when a button is clicked
-    fakeSortSetup();
-  });
-  $("#modifierHeader").click(function() { // This event fires when a button is clicked
-    fakeSortSetup();
-  });
-  $("#modifiedHeader").click(function() { // This event fires when a button is clicked
-    fakeSortSetup();
-  });
 });
 
 function showSuccess() {
   $("#successImage").css('display', 'block');
   $("#mainDialog").css('display', 'none');
   showSpinner(false)
-}
-
-function fakeSortSetup() {
-  showSpinner(true);
-  $(".tableRow").css('display', 'none');
-  setTimeout(fakeSort, 500);
-}
-
-function fakeSort() {
-  showSpinner(false);
-  $(".tableRow").css('display', 'block');
 }
 
 function showSpinner(show) {
@@ -61,7 +28,6 @@ function showSpinner(show) {
 // Your Client ID can be retrieved from your project in the Google
 // Developer Console, https://console.developers.google.com
 var CLIENT_ID = '17222932902-h9p0194vetfj5qtprn6g1dhma472pqdm.apps.googleusercontent.com';
-
 var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
 
 /**
@@ -81,18 +47,18 @@ function checkAuth() {
  *
  * @param {Object} authResult Authorization result.
  */
- function handleAuthResult(authResult) {
-   if (authResult && !authResult.error) {
-     // Hide auth UI, then load client library.
-     loadDriveApi();
-     $("#mainDialog").css('display', 'block');
-     $("#authorize-button").css('display', 'none');
-   } else {
-     showSpinner(false)
-     $("#mainDialog").css('display', 'none');
-     $("#authorize-button").css('display', 'block');
-   }
- }
+function handleAuthResult(authResult) {
+  if (authResult && !authResult.error) {
+    // Hide auth UI, then load client library.
+    loadDriveApi();
+    $("#mainDialog").css('display', 'block');
+    $("#authorize-button").css('display', 'none');
+  } else {
+    showSpinner(false)
+    $("#mainDialog").css('display', 'none');
+    $("#authorize-button").css('display', 'block');
+  }
+}
 
 /**
  * Initiate auth flow in response to user clicking authorize button.
@@ -151,6 +117,7 @@ function listFiles(q) {
         }
 
         addFile(i, file.title, viewedString, modifiedString, lastUser);
+        docIDs.push(file.id)
       }
 
       addTableHandlers();
@@ -159,6 +126,46 @@ function listFiles(q) {
       addFile('No files found.');
     }
   });
+}
+
+function getFileMetadata(fileId) {
+  var request = gapi.client.drive.files.get({
+    'fileId': fileId
+  });
+  request.execute(function(resp) {
+    var link = resp.alternateLink;
+    console.log(resp.embedLink);
+    console.log(resp.id)
+    var thumbnailLink = "https://docs.google.com/feeds/vt?gd=true&id=1enefB2co2WxNWGhiAfx08TyqJzlGM3nzMzFbvMRlrnA&v=0&s=AMedNnoAAAAAVoNNm6DB1T4sqGtGRmsDz4h0ZWe565J3&sz=s220"
+    var owner = resp.ownerNames[0];
+    populateMetadataGrid(link, thumbnailLink, owner);
+  });
+}
+
+function downloadFile(file, callback) {
+  if (file.downloadUrl) {
+    var accessToken = gapi.auth.getToken().access_token;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', file.downloadUrl);
+    xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+    xhr.onload = function() {
+      callback(xhr.responseText);
+    };
+    xhr.onerror = function() {
+      callback(null);
+    };
+    xhr.send();
+  } else {
+    callback(null);
+  }
+}
+
+function populateMetadataGrid(link, thumbnailLink, owner) {
+  $("#ownerMetadata").replaceWith("<p id=\"ownerMetadata\"><b>Owner: </b>" + owner +"</p>");
+  $("#linkMetadata").replaceWith("<p id=\"linkMetadata\"><b>Open in Drive: </b><a href=" + link + " target=\"_blank\">link</a></p>")
+  $("#thumbnail").attr('src', thumbnailLink);
+  $("#previewSpinner").css('display', 'none');
+  $("#metadataContent").css('display', 'block');
 }
 
 /**
@@ -180,17 +187,48 @@ function clearTable() {
   $('#myTable tr').slice(1).remove();
 }
 
-function searchSheets() {
-  var q = $('#searchBox').val();
-  listFiles(q);
+function clearSelected() {
+  $('#connectButton').prop('disabled', true);
+  $("table#myTable tr").removeClass("selected");
+  $("#metadataContent").css('display', 'none');
+  $('#instructions').css('display', 'block');
 }
 
 function addTableHandlers() {
   $("tbody").on("click", "tr", function(e) {
+    var index = $("tr").index($(this));
+    getFileMetadata(docIDs[index]);
     $('#connectButton').prop('disabled', false);
+    fetchMetadata();
     $(this)
        .addClass("selected")
        .siblings(".selected")
        .removeClass("selected");
   });
+
+  $("#search").on("keyup", function() {
+    clearSelected();
+    var value = $(this).val().toLowerCase();
+
+    $("table tr").each(function(index) {
+        if (index !== 0) {
+
+            $row = $(this);
+
+            var title = $row.find("td:first").text().toLowerCase();;
+
+            if (title.indexOf(value) !== 0) {
+                $row.hide();
+            }
+            else {
+                $row.show();
+            }
+        }
+    });
+  });
+}
+
+function fetchMetadata() {
+  $("#previewSpinner").css('display', 'block');
+  $('#instructions').css('display', 'none');
 }
